d6043c6d354fbd20df8d49263ab92b68c2cd76fc
DungTran46 <dung_tran_72@yahoo.com>
2018-06-03 21:42:43 -0700

    Add files via upload

---
+++ libraries/CharactersLib/CharactersLib.cpp
@@ -0,0 +1,26 @@
+#include "CharactersLib.h"
+
+char getLetter(int letter_code)
+{
+	switch (letter_code)
+	{
+		case 1: return 'A'
+			break;
+		case 1: return 'A'
+			break;
+		case 1: return 'A'
+			break;
+		case 1: return 'A'
+			break;
+		case 1: return 'A'
+			break;
+		case 1: return 'A'
+			break;
+		case 1: return 'A'
+			break;
+	}
+}
+char getNumner(int number_code)
+{
+	
+}
---
+++ libraries/CharactersLib/CharactersLib.h
@@ -0,0 +1,5 @@
+#ifndef CHARACTERS_LIB
+#define CHARACTERS_LIB
+
+char getLetter(int letter_code);
+char getNumner(int number_code);
